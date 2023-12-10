0a2732a11f371efeb38b1e9807c562568507e9a2
daym4 <mday4@villanova.edu>
2023-12-10 08:40:55 -0500

    I created the AddRide.js to create a page with a form for users to fill so that they can submit a ride opportunity. When the user presses submit, the form is sent to Firebase and the data is added to the rides collection of the Firestore firebase database I made.

---
+++ src/screens/AddRide.js
@@ -0,0 +1,47 @@
+import {db} from "./../firebase.js";
+import React from 'react';
+
+const AddRide = () => {
+
+	const sub = (e) => {
+
+		e.preventDefault();
+
+        const elementsArray = [...e.target.elements];
+
+        const formData = elementsArray.reduce((accumulator, currentVal) => {
+            if (currentVal.id){
+                accumulator[currentVal.id]= currentVal.value;
+            }
+            return accumulator;
+        }, {})
+       
+        db.collection("rides").add(formData);
+    };
+
+    return (
+        <div className="container">
+            <h1>Add a New Ride</h1>
+            <form onSubmit={sub}>
+                <input type="text" id="fullName" placeholder="Your full name"></input>
+                <br /><br />
+                <input type="text" id="date" placeholder="Enter ride date as : ##/##/####" ></input> 
+                <br /><br />
+                <input type="text" id="time" placeholder="Ride time"></input>
+                <br /><br />
+                <input type="text" id="destination" placeholder="Ride destination"></input>
+                <br /><br />
+                <input type="text" id="email" placeholder="Your email"></input>
+                <br /><br />
+                <input type="text" id="rideType"placeholder="Enter: your car or uber"></input>
+                <br /><br />
+                <input type="text" id="seatsAvailable"placeholder="Number of seats available"></input>
+                <br /><br />
+                <input type="text" id="cost"placeholder="Anticipated total cost of gas/uber to split"></input>       
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddRide;
