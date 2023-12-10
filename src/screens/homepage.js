a9bc2b7d9e6d2a7eb9983ea8a0052227b33fd9c7
daym4 <mday4@villanova.edu>
2023-12-10 08:39:43 -0500

    created homepage.js to be the homepage when the app is opened. This page has the menu across the top of the 4 different pages. It also shows all the rides available that have been posted

---
+++ src/screens/homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Answers from"./../Answers.js";
+
+const HomePage = () => {
+    return (
+        <div>
+            <div className="homepage" >
+                <h1 align="center">Welcome to the Villanova Ride Share Project</h1>
+                <Answers />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
